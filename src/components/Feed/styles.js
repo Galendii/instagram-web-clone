9d57e82dbdfefa03f8d844b9706d5fba7f66bd4d
Breno Zielinski Galendi <brenogalendi@gmail.com>
2020-07-14 10:25:09 -0300

    Fix: [Layout] Width responsivity

--- src/components/Feed/styles.js
+++ src/components/Feed/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 80%;
+  width: 100%;
   margin-top: 40px;
   @media (min-width: 1000px) {
     width: 60%;
--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 100%;
+  min-width: 100%;
+  width: 100vw;
   background: var(--secondary);
   display: flex;
   flex-direction: column;
