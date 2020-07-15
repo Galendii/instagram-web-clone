627db92238a2ff6903cf25296edc41afec4ae408
Breno Zielinski Galendi <brenogalendi@gmail.com>
2020-07-15 00:35:46 -0300

    Update: responsive margin on feed

--- src/components/Body/styles.js
+++ src/components/Body/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   padding: 30px 0;
-  margin: 0 90px 0 90px;
+  margin: 0 30px 0 30px;
   height: 100%;
 
   @media (min-width: 1000px) {
